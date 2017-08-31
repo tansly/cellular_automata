aa2ca1d720695b702274e941ae385f9932d2f19c
Yagmur Oymak <yagmur.oymak@gmail.com>
2017-08-31 19:10:17 +0300

    Sleep between generations for a smoother view

--- src/predator_and_prey/predator_and_prey.cpp
+++ src/predator_and_prey/predator_and_prey.cpp
@@ -1,11 +1,13 @@
 #include "curses/screen.h"
 #include "grid/grid.h"
 
+#include <iostream>
 #include <memory>
 #include <map>
 #include <random>
 #include <stdexcept>
 #include <string>
+#include <unistd.h>
 #include <utility>
 
 class PredatorAndPrey {
@@ -192,7 +194,17 @@ int main(int argc, char **argv)
     Curses::Screen screen;
     PredatorAndPrey automaton(screen.get_max_x(), screen.get_max_y());
     const int max_gen = 10000;
+    int rate = 100000;
+    if (argc == 2) {
+        try {
+            rate = std::stoi(std::string(argv[1]));
+        } catch (const std::invalid_argument &ex) {
+            std::cerr << "Invalid argument" << std::endl;
+            return 1;
+        }
+    }
     while (automaton.get_curr_gen() < max_gen) {
+        usleep(rate);
         automaton.draw(screen);
         automaton.update();
     }
