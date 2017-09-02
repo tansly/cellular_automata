e579932324e748fcee7f431b6cf3dbc52ab501f5
Yagmur Oymak <yagmur.oymak@gmail.com>
2017-09-03 00:45:21 +0300

    Improve the grid classes     Add iterators     Add a method to get the Moore neighbors of a cell

--- src/grid/grid.h
+++ src/grid/grid.h
@@ -8,9 +8,21 @@ namespace Grid {
 template <class T>
 class Grid {
 public:
+    typedef typename std::vector<T>::iterator GridIterator;
+
     Grid(int size_x_, int size_y_);
-    T &operator()(int x, int y);
-    const T &operator()(int x, int y) const;
+    virtual ~Grid();
+
+    virtual T &operator()(int x, int y);
+    virtual const T &operator()(int x, int y) const;
+    /* Get the Moore neigbors of a cell, omitting the center */
+    virtual std::vector<T> moore_neighbors(const GridIterator &it) const;
+
+
+    auto begin();
+    auto begin() const;
+    auto end();
+    auto end() const;
 
     int get_size_x() const;
     int get_size_y() const;
@@ -29,6 +41,11 @@ Grid<T>::Grid(int size_x_, int size_y_) :
 }
 
 template <class T>
+Grid<T>::~Grid()
+{
+}
+
+template <class T>
 T &Grid<T>::operator()(int x, int y)
 {
     return vec[x * size_y + y];
@@ -38,6 +55,50 @@ template <class T>
 const T &Grid<T>::operator()(int x, int y) const
 {
     return vec[x * size_y + y];
+}
+
+template <class T>
+auto Grid<T>::begin()
+{
+    return vec.begin();
+}
+
+template <class T>
+auto Grid<T>::begin() const
+{
+    return vec.begin();
+}
+
+template <class T>
+auto Grid<T>::end()
+{
+    return vec.end();
+}
+
+template <class T>
+auto Grid<T>::end() const
+{
+    return vec.end();
+}
+
+template <class T>
+std::vector<T> Grid<T>::moore_neighbors(const GridIterator &it) const
+{
+    std::vector<T> neighbors;
+    int v_pos = it - this->begin();
+    int x_pos = v_pos / size_y;
+    int y_pos = v_pos % size_y;
+    for (int x = -1; x <= 1; ++x) {
+        for (int y = -1; y <= 1; ++y) {
+            if (x_pos + x < 0 || x_pos + x >= size_x ||
+                    y_pos + y < 0 || y_pos + y >= size_y ||
+                    (x == 0 && y == 0)) {
+                continue;
+            }
+            neighbors.push_back(this->operator()(x_pos + x, y_pos + y));
+        }
+    }
+    return neighbors;
 }
 
 template <class T>
--- src/grid/toroidal_grid.h
+++ src/grid/toroidal_grid.h
@@ -11,13 +11,22 @@ template <class T>
 class ToroidalGrid : public Grid<T> {
 public:
     ToroidalGrid(int size_x_, int size_y_);
-    T &operator()(int x, int y);
-    const T &operator()(int x, int y) const;
+    virtual ~ToroidalGrid() override;
+
+    virtual T &operator()(int x, int y) override;
+    virtual const T &operator()(int x, int y) const override;
+    /* Get the Moore neigbors of a cell, omitting the center */
+    virtual std::vector<T> moore_neighbors(const typename Grid<T>::GridIterator &it) const override;
 };
 
 template <class T>
 ToroidalGrid<T>::ToroidalGrid(int size_x_, int size_y_) :
     Grid<T>(size_x_, size_y_)
+{
+}
+
+template <class T>
+ToroidalGrid<T>::~ToroidalGrid()
 {
 }
 
@@ -39,6 +48,24 @@ const T &ToroidalGrid<T>::operator()(int x, int y) const
     x = ((x % nx) + nx) % nx;
     y = ((y % ny) + ny) % ny;
     return Grid<T>::operator()(x, y);
+}
+
+template <class T>
+std::vector<T> ToroidalGrid<T>::moore_neighbors(const typename Grid<T>::GridIterator &it) const
+{
+    std::vector<T> neighbors;
+    int v_pos = it - this->begin();
+    int x_pos = v_pos / this->get_size_y();
+    int y_pos = v_pos % this->get_size_y();
+    for (int x = -1; x <= 1; ++x) {
+        for (int y = -1; y <= 1; ++y) {
+            if (x == 0 && y == 0) {
+                continue;
+            }
+            neighbors.push_back(this->operator()(x_pos + x, y_pos + y));
+        }
+    }
+    return neighbors;
 }
 
 };
