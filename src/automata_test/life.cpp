af62906a13ba1297de2b16381df96047f1bdfe87
Yagmur Oymak <yagmur.oymak@gmail.com>
2018-04-09 01:11:36 +0300

    batch compute generations

--- src/automata_test/life.cpp
+++ src/automata_test/life.cpp
@@ -24,24 +24,17 @@
 #include <string>
 #include <unistd.h>
 
-int main(int argc, char **argv)
+int main(void)
 {
     Curses::Screen screen;
     Automata::Life automaton(screen.get_max_x(), screen.get_max_y());
-    const int max_gen = 10000;
-    int rate = 100000;
-    if (argc == 2) {
-        try {
-            rate = std::stoi(std::string(argv[1]));
-        } catch (const std::invalid_argument &ex) {
-            std::cerr << "Invalid argument" << std::endl;
-            return 1;
+
+    for (;;) {
+        automaton.draw(screen);
+        for (int i = 0; i < 101; ++i) {
+            automaton.update();
         }
     }
-    while (automaton.get_curr_gen() < max_gen) {
-        usleep(rate);
-        automaton.draw(screen);
-        automaton.update();
-    }
+
     return 0;
 }
